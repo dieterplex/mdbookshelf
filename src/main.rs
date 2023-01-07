083d976656bb4fdc905a7c566332fb2e4e505b39
d1t2 <dieterplex@gmail.com>
2023-01-08 03:04:57 +0800

    Update deps: mdbook-epub 0.4.25 and fixes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use std::ffi::OsString;
 use std::path::{Path, PathBuf};
 use std::process;
 
-use clap::{crate_version, App, Arg};
+use clap::{crate_version, Arg, ArgAction, Command};
 use env_logger::Env;
 use log::{error, info};
 use mdbookshelf::config::Config;
@@ -14,33 +15,33 @@ fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     color_backtrace::install();
 
-    let matches = App::new("mdbookshelf")
+    let matches = Command::new("mdbookshelf")
         .about("Executes mdbook-epub on a collection of repositories")
         .version(concat!("v", crate_version!()))
         .author("Ramses Ladlani <rladlani@gmail.com>")
         .arg(
-            Arg::with_name("working_dir")
+            Arg::new("working_dir")
                 .short('w')
                 .long("working_dir")
                 .value_name("WORKING_DIR")
                 .help("Sets a custom working directory where the book repositories will be cloned")
-                .takes_value(true),
+                .action(ArgAction::Set),
         )
         .arg(
-            Arg::with_name("destination_dir")
+            Arg::new("destination_dir")
                 .short('d')
                 .long("destination_dir")
                 .value_name("DESTINATION_DIR")
                 .help("Sets the destination directory")
-                .takes_value(true),
+                .action(ArgAction::Set),
         )
         .arg(
-            Arg::with_name("templates_dir")
+            Arg::new("templates_dir")
                 .short('t')
                 .long("templates_dir")
                 .value_name("TEMPLATES_DIR")
                 .help("Sets the templates directory (if not set, will generate manifest.json)")
-                .takes_value(true),
+                .action(ArgAction::Set),
         )
         .get_matches();
 
@@ -54,7 +55,7 @@ fn main() {
         Config::default()
     };
 
-    if let Some(destination_dir) = matches.value_of("destination_dir") {
+    if let Some(destination_dir) = matches.get_one::<OsString>("destination_dir") {
         config.destination_dir = Some(PathBuf::from(destination_dir));
     }
 
@@ -68,7 +69,7 @@ fn main() {
         config.destination_dir.as_ref().unwrap().display()
     );
 
-    if let Some(working_dir) = matches.value_of("working_dir") {
+    if let Some(working_dir) = matches.get_one::<OsString>("working_dir") {
         config.working_dir = Some(PathBuf::from(working_dir));
     }
 
@@ -79,7 +80,7 @@ fn main() {
         config.working_dir.as_ref().unwrap().display()
     );
 
-    if let Some(templates_dir) = matches.value_of("templates_dir") {
+    if let Some(templates_dir) = matches.get_one::<OsString>("templates_dir") {
         config.templates_dir = Some(PathBuf::from(templates_dir));
     }
 
